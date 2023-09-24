96061e0cf471fc80733815d6b615daf5a98c10e2
Carter <carterleebainter@gmail.com>
2023-09-24 12:22:34 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+        System.out.println("Enter your Age: ");
+        age = in.nextInt();
+
+        if (age >= 21)
+        {
+            System.out.println("You get a paper wristband!");
+        
+    }
+}
